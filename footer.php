6a61fe02e9d8079b57b5301eacc2640fca9d66d0
JaxsonWang <wangjin@boxrom.com>
2017-05-10 19:35:28 +0800

    新增功能

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
 <!-- Footer -->
 <footer id="footer" itemscope="itemscope" itemtype="https://schema.org/WPFooter">
     <p class="copyright">
-	    <?php esc_attr_e('Copyright ©'); ?><?php esc_attr_e(date('Y')); ?> . <span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span> . Theme by <a href="#" rel="external nofollow" target="_blank">Her</a><br />
-        <a href="<?php echo esc_url( __( 'https://wordpress.org/' ) ); ?>"><?php printf( __( 'Powered by %s' ), 'WordPress' ); ?></a> .
+	    <?php esc_attr_e('Copyright ©'); ?><?php esc_attr_e(date('Y')); ?> . <span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span> . <a href="<?php echo esc_url( __( 'https://wordpress.org/' ) ); ?>"><?php printf( __( 'Powered by %s' ), 'WordPress' ); ?></a><br />
+        Theme by <a href="https://www.banxia.me" rel="external nofollow" target="_blank">Her</a> . Make By <a href="https://iiong.com" rel="external nofollow" target="_blank">淮城一只猫</a><br/>
         <?php if (get_option('theme_icp_num') != '' ) {echo '<a href="http://www.miitbeian.gov.cn" rel="external nofollow" target="_blank">'. get_option('theme_icp_num') .'</a><br/>';}  ?>
         <?php if (get_option('theme_blog_footer_code') != '' ) { echo get_option('theme_blog_footer_code'); } ?>
     </p>
